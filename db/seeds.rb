d708194e30921625f371b7c4df7d1b8cbbfdc0eb
my_name <my_name@example.com>
2020-07-06 21:47:53 +0900

    a

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |n|
+    User.create!(
+      email: "test#{n + 1}@gmail.com",
+      name: "ケントマンNO.#{n + 1}",
+      password: "password"
+    )
+
+    User.create!(
+        email: "sample#{n + 1}@test.com",
+        name: "山田太郎#{n + 1}",
+        password: "password"
+    )
+
+    User.create!(
+        email: "sample#{n + 1}@mail.com",
+        name: "山田花子#{n + 1}",
+        password: "password"
+    )
+
+    User.create!(
+        email: "sample#{n + 1}@email.com",
+        name: "タズキングNO.#{n + 1}",
+        password: "password"
+    )
+  end
